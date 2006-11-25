ebaae99baf862c1e47b0b00b3caa17e7aa3c041d
seninp <seninp@5003ad82-3f21-0410-b9e6-d97d5e13bc38>
2006-11-25 19:09:07 +0000

    Working on MRF

--- src/edu/hawaii/senin/rjimage/model/ImageFactory.java
+++ src/edu/hawaii/senin/rjimage/model/ImageFactory.java
@@ -1,13 +1,21 @@
 package edu.hawaii.senin.rjimage.model;
 
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
 import java.awt.image.Raster;
+import java.awt.image.SampleModel;
 import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Observable;
+import java.util.TreeMap;
 
 import javax.imageio.ImageIO;
 
@@ -33,13 +41,23 @@ public class ImageFactory extends Observable {
    */
   private String imageFileName;
   /**
-   * Holds Gaussians for the image segmentation.
+   * Holds Gaussians for the image segmentation, i.e. class label is a key and IClass object holds
+   * all parameters for the distribution itself.
    */
-  private ArrayList<IClass> classes;
+  private TreeMap<Integer, IClass> classes;
 
-  private Segmentation segmentation;
-
+  /**
+   * Holds array of labels.
+   */
   private Integer[][] labels;
+  /**
+   * Holds raster USHORT type
+   */
+  private short raster[][];
+  /**
+   * Holds beta value.
+   */
+  private Double beta = 0.28D;
 
   /**
    * Resets labeling with initial values. See Kato99Bayesian explanation.
@@ -49,8 +67,8 @@ public class ImageFactory extends Observable {
     if (this.classes.size() > 0) {
       this.classes.clear();
     }
-    this.classes.add(new IClass(0.33, 0.33, 0.5));
-    this.classes.add(new IClass(0.66, 0.66, 0.5));
+    this.classes.add(new IClass(0.33, 0.3, 0.5));
+    this.classes.add(new IClass(0.66, 0.3, 0.5));
   }
 
   /**
@@ -76,6 +94,9 @@ public class ImageFactory extends Observable {
     this.classes = new ArrayList<IClass>();
     try {
       originalImage = ImageIO.read(new File(imageFileName));
+      // originalImage = convertToGrayscale(ImageIO.read(new File(imageFileName)));
+      // int imageType = originalImage.getType();
+      // int imageType = image.getType();
       currentImage = new BufferedImage(originalImage.getColorModel(), originalImage.getRaster(),
           originalImage.isAlphaPremultiplied(), null);
     }
@@ -96,8 +117,15 @@ public class ImageFactory extends Observable {
     this.classes = new ArrayList<IClass>();
     try {
       originalImage = ImageIO.read(selectedFile);
-      currentImage = new BufferedImage(originalImage.getColorModel(), originalImage.getRaster(),
-          originalImage.isAlphaPremultiplied(), null);
+      int imageType = originalImage.getType();
+      if (imageType != BufferedImage.TYPE_BYTE_GRAY) {
+        setChanged();
+        notifyObservers(ImageFactoryStatus.INVALID_IMAGE);
+      }
+      else {
+        currentImage = new BufferedImage(originalImage.getColorModel(), originalImage.getRaster(),
+            false, null);
+      }
     }
     catch (IOException e) {
       // TODO Auto-generated catch block
@@ -130,26 +158,94 @@ public class ImageFactory extends Observable {
     // resegment
     resegmentImage();
     // create new image
-    WritableRaster writableRaster = this.originalImage.getRaster();
-    Integer increment = 255 / this.classes.size();
-    Integer height = writableRaster.getHeight();
-    Integer width = writableRaster.getWidth();
-    double[] pixel = new double[1];
-    for (int i = 0; i < height; i++) {
-      for (int j = 0; j < width; j++) {
-        pixel[0] = (this.labels[i][j] + 1) * increment;
-        writableRaster.setPixel(i, j, pixel);
-      }
-    }
-    this.currentImage = new BufferedImage(originalImage.getColorModel(), writableRaster,
-        originalImage.isAlphaPremultiplied(), null);
+    this.currentImage = generateSegmentedImage();
     setChanged();
     notifyObservers(ImageFactoryStatus.NEW_SEGMENTATION);
+  }
+
+  private BufferedImage generateSegmentedImage() {
+
+    Raster raster = this.originalImage.getRaster();
+    Integer height = raster.getHeight();
+    Integer width = raster.getWidth();
+
+    byte[] rasterPlain = new byte[height * width];
+
+    Integer increment = 255 / this.classes.size();
+
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        Integer value = this.labels[y][x] * increment;
+        rasterPlain[y * width + x] = value.byteValue();
+      }
+    }
+    // ComponentColorModel ccm = new ComponentColorModel(ColorSpace.getInstance(ColorSpace.CS_GRAY),
+    // new int[] { 255 }, false, false, Transparency.OPAQUE, DataBuffer.TYPE_USHORT);
+    ColorModel ccm = this.originalImage.getColorModel();
+    // ComponentSampleModel csm = new ComponentSampleModel(DataBuffer.TYPE_USHORT, width, height, 1,
+    // width, new int[] { 0 });
+    SampleModel csm = this.originalImage.getSampleModel();
+    DataBuffer dataBuf = new DataBufferByte(rasterPlain, width);
+    WritableRaster wr = Raster.createWritableRaster(csm, dataBuf, new Point(0, 0));
+    Hashtable<String, String> ht = new Hashtable<String, String>();
+    ht.put("owner", "Senin Pavel");
+    return new BufferedImage(ccm, wr, true, ht);
 
   }
 
   public Image getCurrentImage() {
     return this.currentImage;
+  }
+
+  public BufferedImage toGrayScale(BufferedImage image) {
+    BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(),
+        BufferedImage.TYPE_USHORT_GRAY);
+    Graphics2D g = result.createGraphics();
+    g.drawRenderedImage(image, null);
+    g.dispose();
+
+    return result;
+  }
+
+  private Double getLocalEnergy(Integer x, Integer y, Integer label) {
+    return Singleton(x, y, label) + Doubleton(x, y, label);
+  }
+
+  private Double Singleton(Integer x, Integer y, Integer label) {
+    Double m = this.classes.get(label).getMean();
+    Double s = this.classes.get(label).getStDev();
+
+    return Math.log(Math.sqrt(2.0 * Math.PI * s) + Math.pow(this.raster[x][y] - m, 2)) / (2.0 * s);
+  }
+
+  private Double Doubleton(int x, int y, int label)
+  {
+    Integer height = this.raster.length;
+    Integer width = this.raster[0].length;
+    
+    double energy = 0.0;
+
+    if (y!=height-1) // south
+      {
+        if (label == this.labels[x][x]) energy -= beta;
+        else energy += beta;
+      }
+    if (x!=width-1) // east
+      {
+        if (label == this.labels[][j+1]) energy -= beta;
+        else energy += beta;
+      }
+    if (i!=0) // nord
+      {
+        if (label == classes[i-1][j]) energy -= beta;
+        else energy += beta;
+      }
+    if (j!=0) // west
+      {
+        if (label == classes[i][j-1]) energy -= beta;
+        else energy += beta;
+      }
+    return energy;
   }
 
 }
--- src/edu/hawaii/senin/rjimage/view/View.java
+++ src/edu/hawaii/senin/rjimage/view/View.java
@@ -14,10 +14,9 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.border.BevelBorder;
 
+import net.miginfocom.swing.MigLayout;
 import edu.hawaii.senin.rjimage.model.ImageFactory;
 import edu.hawaii.senin.rjimage.model.ImageFactoryStatus;
-
-import net.miginfocom.swing.MigLayout;
 
 public class View implements Observer {
 
@@ -32,6 +31,7 @@ public class View implements Observer {
   private JLabel originalImageLabel = new JLabel(originalImageIcon);
   private ImageIcon currentImageIcon = new ImageIcon();
   private JLabel currentImageLabel = new JLabel(originalImageIcon);
+  private StringBuffer logText = new StringBuffer(10000);
 
   public View() {
     // does nothing
@@ -59,33 +59,37 @@ public class View implements Observer {
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     // set the layout for the application
-    MigLayout layout = new MigLayout();
+    MigLayout layout = new MigLayout("", "[center]50[center]", "[]10[]10[]");
     this.frame.getContentPane().setLayout(layout);
 
     // add and set basic settings for the all panels
     //
     // original Image display panel
-    this.frame.getContentPane().add(originalImagePane);
+    this.frame.getContentPane().add(originalImagePane, "width :200:, height :200:");
     originalImagePane.setBorder(BorderFactory.createEtchedBorder(BevelBorder.LOWERED));
     originalImagePane.add(originalImageLabel);
 
     // current segmented image panel
-    this.frame.getContentPane().add(currentImagePane);
+    this.frame.getContentPane().add(currentImagePane, "width :200:, height :200:, wrap");
     currentImagePane.setBorder(BorderFactory.createEtchedBorder(BevelBorder.LOWERED));
     currentImagePane.add(currentImageLabel);
-
-    // adding logging panel
-    this.frame.getContentPane().add(logPane);
-    logPane.setViewportView(logTextPane);
-    logTextPane.setText("logging:");
-
-    // add start button
-    this.frame.getContentPane().add(startProcessButton);
-    startProcessButton.setBounds(133, 210, 105, 28);
 
     // add load image button
     this.frame.getContentPane().add(loadImageButton);
     loadImageButton.setBounds(7, 210, 119, 28);
+
+    // add start button
+    this.frame.getContentPane().add(startProcessButton, "wrap");
+    startProcessButton.setBounds(133, 210, 105, 28);
+
+    // adding logging panel
+    this.frame.getContentPane().add(logTextPane, "span");
+    // logPane.setLayout(new MigLayout());
+    // logPane.setViewportView(logTextPane,);
+    logTextPane.setBorder(BorderFactory.createEtchedBorder(BevelBorder.LOWERED));
+    logTextPane.setAutoscrolls(true);
+    this.logText.append("ReversibleJump MCMC Image segmentation ver. 1.0 \n logging:\n");
+    logTextPane.setText(this.logText.toString());
 
     // Show frame
     this.frame.pack();
@@ -105,8 +109,7 @@ public class View implements Observer {
   public void update(Observable arg0, Object arg1) {
     System.out.println(arg0.toString() + arg1.toString());
     // if reported loading of new image
-    if ((arg0 instanceof ImageFactory)
-        && ((Integer)arg1).equals(ImageFactoryStatus.NEW_IMAGE)) {
+    if ((arg0 instanceof ImageFactory) && ((Integer) arg1).equals(ImageFactoryStatus.NEW_IMAGE)) {
       if (null == ((ImageFactory) arg0).getOriginalImage()) {
         assert true;
       }
@@ -115,16 +118,16 @@ public class View implements Observer {
         originalImageLabel.setIcon(originalImageIcon);
         originalImageLabel.updateUI();
         this.originalImagePane.repaint();
-        
+
         currentImageIcon.setImage(((ImageFactory) arg0).getCurrentImage());
         currentImageLabel.setIcon(currentImageIcon);
         currentImageLabel.updateUI();
         this.currentImagePane.repaint();
       }
     }
-    
+
     if ((arg0 instanceof ImageFactory)
-        && ((Integer)arg1).equals(ImageFactoryStatus.NEW_SEGMENTATION)) {
+        && ((Integer) arg1).equals(ImageFactoryStatus.NEW_SEGMENTATION)) {
       if (null == ((ImageFactory) arg0).getOriginalImage()) {
         assert true;
       }
@@ -134,6 +137,10 @@ public class View implements Observer {
         currentImageLabel.updateUI();
         this.currentImagePane.repaint();
       }
+    }
+
+    if ((arg0 instanceof ImageFactory) && ((Integer) arg1).equals(ImageFactoryStatus.INVALID_IMAGE)) {
+      this.logText.append("ERROR: Invalid Image selected!\n");
     }
   }
 }
